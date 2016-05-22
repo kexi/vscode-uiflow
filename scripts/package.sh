5540b0ca37d3998b0c6319cc226118ab91754691
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-05-22 23:33:34 +0900

    fix

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+BRANCH="release-v0.0.1"
 npm install -g vsce
-cd client && vsce package
+cd client && vsce package --baseImagesUrl https://github.com/kexi/vscode-uiflow/blob/$BRANCH/client/
