8078292af542e9a07c43302b6114231a049d5650
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-05-29 00:29:39 +0900

    suggest

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -5,6 +5,7 @@ import { activate as activateClient } from './client';
 import { activate as activatePreview } from './preview';
 import { activate as activateExport } from './export';
 import { activate as activateDocumentSymbolProvider } from './outline';
+import { activate as activateCompletionItemProvider } from './suggest';
 import { MODE } from './mode';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -12,6 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	activatePreview(context);
 	activateExport(context);
 	activateDocumentSymbolProvider(context);
+	activateCompletionItemProvider(context);
 
 	let diagnosticCollection = vscode.languages.createDiagnosticCollection(MODE.language);
 	context.subscriptions.push(diagnosticCollection);
---
+++ client/src/suggest.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import { CancellationToken, CompletionList, CompletionItemKind, CompletionItem, CompletionItemProvider, ExtensionContext, TextDocument, Position} from 'vscode';
+import { Compiler, CompileFormat } from './compiler';
+import { MODE } from './mode';
+
+export function activate(context: ExtensionContext) {
+	let registration = vscode.languages.registerCompletionItemProvider(MODE, new UiflowCompletionItemProvider());
+	context.subscriptions.push(registration);
+}
+
+export class UiflowCompletionItemProvider implements CompletionItemProvider {
+	public provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Thenable<CompletionItem[]> {
+		let lineText = document.lineAt(position.line);
+		if (!lineText.text.substring(0, position.character).match(/=.*=>/)) {
+			return Promise.resolve([]);
+		}
+		return Compiler.compile('', document.getText(), CompileFormat.JSON).then(buffer => {
+			let json = JSON.parse(String(buffer));
+			let list: CompletionItem[] = [];
+			Object.keys(json).forEach(key => {
+				let segment: {name: string} = json[key];
+				let item = new CompletionItem(segment.name);
+				item.kind = CompletionItemKind.Class;
+				list.push(item);
+			});
+			return list;
+		});
+	}
+}
