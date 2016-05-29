e30ed54d152c5d83b5952457c26e107aac7cbd67
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-05-29 16:21:06 +0900

    fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,4 +2,4 @@
 
 BRANCH="release/v0.0.3"
 npm install -g vsce
-cd client && vsce publish --baseImagesUrl https://github.com/kexi/vscode-uiflow/blob/$BRANCH/client/
+cd client && vsce publish --baseImagesUrl https://raw.githubusercontent.com/kexi/vscode-uiflow/release/$BRANCH/client/
