c49edc41f8508638c0de3131656c427402bf88d4
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-06-26 19:23:26 +0900

    fix

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-BRANCH="release/v0.0.5"
+BRANCH="release/v0.0."
 npm install -g vsce
 cd client && vsce package --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$BRANCH/
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-BRANCH="release/v0.0.5"
+BRANCH="release/v0.0.6"
 vsce publish --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$BRANCH/
