82f4fbbb24332f370f1a63a8b42c04812348ebe5
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-06-26 19:19:45 +0900

    fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 BRANCH="release/v0.0.5"
-vsce publish --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$BRANCH/client/
+vsce publish --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$BRANCH/
