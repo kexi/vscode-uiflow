41e80bf6273e3e69dcd334d45f518205fc247f37
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-07-10 12:35:57 +0900

    fix

--- src/preview.ts
+++ src/preview.ts
@@ -7,18 +7,15 @@ import { CompileFormat, Compiler } from './compiler';
 import { MODE } from './mode';
 
 const command = 'uiflow.openPreview';
-const previewUri = 'uiflow-preview://authority/uiflow-preview';
-const scheme = 'uiflow-preview';
+const scheme = 'uiflow';
 
 class UiflowTextDocumentContentProvider implements TextDocumentContentProvider {
 	private _onDidChange = new EventEmitter<Uri>();
 
 	public provideTextDocumentContent(uri: Uri): string | Thenable<string> {
-		let editor = window.activeTextEditor;
-		if (editor.document.languageId !== MODE.language) {
-			return 'Active window does not show a Uiflow document - No properties to preview.';
-		}
-		return this.createPreview();
+		return vscode.workspace.openTextDocument(Uri.parse(uri.query)).then(doc => {
+			return this.render(doc);
+		});
 	}
 
 	get onDidChange(): Event<Uri> {
@@ -29,11 +26,10 @@ class UiflowTextDocumentContentProvider implements TextDocumentContentProvider {
 		this._onDidChange.fire(uri);
 	}
 
-	private createPreview(): string | Thenable<string> {
-		let editor = window.activeTextEditor;
-		let code = editor.document.getText();
+	private render(document: vscode.TextDocument): string | Thenable<string> {
+		let code = document.getText();
 		let promise = new Promise((resolve, rejected) => {
-			Compiler.compile(editor.document.uri.path, code, CompileFormat.SVG)
+			Compiler.compile(document.uri.path, code, CompileFormat.SVG)
 				.then(
 					buffer => resolve(fixFont(String(buffer))),
 					reason => rejected(reason));
@@ -43,23 +39,25 @@ class UiflowTextDocumentContentProvider implements TextDocumentContentProvider {
 }
 
 export function activate(context: ExtensionContext) {
-	let uri = vscode.Uri.parse(previewUri);
 	let provider = new UiflowTextDocumentContentProvider();
 	vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
 		if (event.document === vscode.window.activeTextEditor.document) {
-			provider.update(uri);
-		}
-	});
-	vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
-		if (e.textEditor === vscode.window.activeTextEditor) {
-			provider.update(uri);
+			provider.update(getUiflowUri(event.document.uri));
 		}
 	});
 	let registration = workspace.registerTextDocumentContentProvider(scheme, provider);
 	let openDiagram = vscode.commands.registerCommand(
-		command,
-		() => vscode.commands.executeCommand('vscode.previewHtml', uri, vscode.ViewColumn.Two));
+		command, uri => showPreview(uri), vscode.ViewColumn.Two);
 	context.subscriptions.push(openDiagram, registration);
+}
+
+function showPreview(uri: Uri) {
+	if (!(uri instanceof Uri)) {
+		if (vscode.window.activeTextEditor) {
+			uri = vscode.window.activeTextEditor.document.uri;
+		}
+	}
+	return vscode.commands.executeCommand('vscode.previewHtml', getUiflowUri(uri), vscode.ViewColumn.Two);
 }
 
 function fixFont(svg: string): string {
@@ -71,4 +69,12 @@ function fixFont(svg: string): string {
 	let size = m[2];
 	let style = `<style>text { font-size: ${size}; font-famiy: ${family}; }</style>`;
 	return svg.replace('<svg', style + '<svg');
+}
+
+function getUiflowUri(uri: any): Uri {
+	return uri.with({
+		scheme: scheme,
+		path: uri.path + '.rendered',
+		query: uri.toString()
+	});
 }
