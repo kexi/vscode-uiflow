90a253a1624f9ab5cdc51ccc3a337e98e156a2fa
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-08-22 17:34:41 +0900

    change baseImagesUrl

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-BRANCH="release/v0.1.0"
+TAG="v0.1.0"
 npm install -g vsce
-vsce package --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$BRANCH/
+vsce package --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$TAG/
