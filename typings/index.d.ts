ee86ee61881e0c798120bbc7513930ecc858af2e
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-10-03 19:17:55 +0900

    mod: typescript 2.0

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/fs-extra/index.d.ts" />
-/// <reference path="globals/parsimmon/index.d.ts" />
-/// <reference path="globals/through2/index.d.ts" />
-/// <reference path="globals/tmp/index.d.ts" />
