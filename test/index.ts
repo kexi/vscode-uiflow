dfcdc8a887ef70ffcfd2536bc29602d616cfd1d7
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-10-08 18:27:01 +0900

    refactor: test timeout from 5000 to default

--- test/index.ts
+++ test/index.ts
@@ -16,8 +16,7 @@ let testRunner = require('vscode/lib/testrunner');
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
 testRunner.configure({
 	ui: 'tdd', 		 // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-	useColors: true, // colored output from test results
-	timeout: 5000
+	useColors: true  // colored output from test results
 });
 
 module.exports = testRunner;
