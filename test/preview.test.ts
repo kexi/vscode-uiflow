66e77ec6b312c55254e9dd9c5f929afabc98062c
Kei Nakayama <kei.of.nakayama@gmail.com>
2018-11-12 21:28:13 +0900

    try

--- test/preview.test.ts
+++ test/preview.test.ts
@@ -18,14 +18,11 @@ suite('UiFlow Extension Tests', () => {
 		fs.copyFileSync(path.join(fixtureSourcePath, 'ok.uif'), path.join(fixturePath, 'ok.uif'));
 	});
 
-	test('Open Preview Side By Side', async (done) => {
+	test('Open Preview Side By Side', async () => {
 		let uri = vscode.Uri.file(path.join(fixturePath, 'ok.uif'));
 		let doc = await vscode.workspace.openTextDocument(uri);
 		await vscode.window.showTextDocument(doc);
-		setTimeout(() => {
-			vscode.commands.executeCommand('uiflow.openPreviewSideBySide');
-			done();
-		}, 3000);
+		await vscode.commands.executeCommand('uiflow.openPreviewSideBySide');
 	});
 
 	// test('Open Preview In Place', async () => {
