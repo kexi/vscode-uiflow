e843f5f5515c53eae3ab63c79d16dc1046983953
Kei Nakayama <kei.of.nakayama@gmail.com>
2018-11-16 19:36:32 +0900

    fix: rename test

--- test/rename.test.ts
+++ test/rename.test.ts
@@ -20,25 +20,20 @@ suite('UiFlow Rename Tests', () => {
 		fs.copyFileSync(path.join(fixtureSourcePath, 'rename.uif'), path.join(fixturePath, 'rename.uif'));
 	});
 
-	test('Test provideRenameEdits#UiflowRenameProvider', done => {
-		vscode.workspace.openTextDocument(path.join(fixturePath, 'rename.uif'))
-		.then(doc => {
-			let position = new vscode.Position(4, 4);
-			let newName = 'sec1';
-			let provider = new UiflowRenameProvider();
-			return provider.provideRenameEdits(doc, position, newName, null)
-			.then((we: vscode.WorkspaceEdit) => {
-				let expected: vscode.Range[] = [
-					new vscode.Range(0, 1, 0, 8),
-					new vscode.Range(4, 4, 4, 11),
-					new vscode.Range(10, 4, 10, 11)
-				];
-				let edits = we.get(doc.uri);
-				expected.forEach((range, i) => {
-					assert.ok(range.isEqual(edits[i].range), `Range should be equal.`);
-				});
-			});
-		}, reason => assert.ok(false, `Error in openTextDocument ${reason}.`))
-		.then(() => done(), reason => done(reason));
+	test('Test provideRenameEdits#UiflowRenameProvider', async () => {
+		const doc = await vscode.workspace.openTextDocument(path.join(fixturePath, 'rename.uif'));
+		const position = new vscode.Position(4, 4);
+		const newName = 'sec1';
+		const provider = new UiflowRenameProvider();
+		const we = await provider.provideRenameEdits(doc, position, newName, null);
+		const expected: vscode.Range[] = [
+			new vscode.Range(0, 1, 0, 8),
+			new vscode.Range(4, 4, 4, 11),
+			new vscode.Range(10, 4, 10, 11)
+		];
+		const edits = we.get(doc.uri);
+		expected.forEach((range, i) => {
+			assert.ok(range.isEqual(edits[i].range), `Range should be equal.`);
+		});
 	});
 });
