012ac3b5e4fe7ae39e079082040d9d9503339fce
Kei Nakayama <kei.of.nakayama@gmail.com>
2018-11-16 19:11:32 +0900

    trigger-characters

--- test/suggest.test.ts
+++ test/suggest.test.ts
@@ -20,26 +20,22 @@ suite('UiFlow Suggest Tests', () => {
 		fs.copyFileSync(path.join(fixtureSourcePath, 'suggest.uif'), path.join(fixturePath, 'suggest.uif'));
 	});
 
-	test('Test provideCompletionItems#UiflowCompletionItemProvider', done => {
-		vscode.workspace.openTextDocument(path.join(fixturePath, 'suggest.uif'))
-			.then(doc => {
-				let testCases: [vscode.Position, string[]][] = [
-					[new vscode.Position(13, 3), ['seg1', 'seg2', 'seg3']],
-					[new vscode.Position(13, 2), []],
-					[new vscode.Position(8, 7), ['seg1', 'seg2', 'seg3']],
-					[new vscode.Position(8, 6), []]
-				];
-				let instance = new suggest.UiflowCompletionItemProvider();
-				let promises = testCases.map(([pos, exp]) => instance.provideCompletionItems(doc, pos, null)
-				.then(list => {
-					assert.equal(list.length, exp.length, `List length must be ${exp.length}`);
-					let items = list.map(l => l.label);
-					for (let i of exp) {
-						assert.ok(items.indexOf(i) >= 0, `Item(${i}) not found in list.`);
-					}
-				}));
-				return Promise.all(promises);
-			}, reason => assert.ok(false, `Error in openTextDocument ${reason}.`))
-			.then(() => done(), reason => done(reason));
+	test('Test provideCompletionItems#UiflowCompletionItemProvider', async () => {
+		const doc = await vscode.workspace.openTextDocument(path.join(fixturePath, 'suggest.uif'));
+		const testCases: [vscode.Position, string[]][] = [
+			[new vscode.Position(13, 3), ['seg1', 'seg2', 'seg3']],
+			[new vscode.Position(13, 2), []],
+			[new vscode.Position(8, 7), ['seg1', 'seg2', 'seg3']],
+			[new vscode.Position(8, 6), []]
+		];
+		const instance = new suggest.UiflowCompletionItemProvider();
+		testCases.map(([pos, exp]) => {
+			const list = instance.provideCompletionItems(doc, pos, null);
+			assert.equal(list.length, exp.length, `List length must be ${exp.length}`);
+			const items = list.map(l => l.label);
+			for (const i of exp) {
+				assert.ok(items.indexOf(i) >= 0, `Item(${i}) not found in list.`);
+			}
+		});
 	});
 });
