7a1e85b3b238b1207c7796df5e6c030c1ec1a139
Kei Nakayama <kei.of.nakayama@gmail.com>
2019-03-19 16:42:16 +0900

    bump version

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAG="v0.9.1"
+TAG="v0.9.2"
 nvm use
 npm install
 vsce package --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$TAG/
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-TAG="v0.9.1"
+TAG="v0.9.2"
 vsce publish --baseImagesUrl https://github.com/kexi/vscode-uiflow/raw/$TAG/
