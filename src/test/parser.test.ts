8750cbbb21cbb88fe3c07760b3362a8dc6f47799
Kei Nakayama <kei.of.nakayama@gmail.com>
2022-03-21 17:33:29 +0900

    fix

--- src/test/parser.test.ts
+++ src/test/parser.test.ts
@@ -12,22 +12,22 @@ act1
 ={api}=>direction1`;
 		let nodes = parser.parse(code);
 		let section = nodes.find(n => n.label === 'section');
-		assert.equal(section.text, 'sec1');
-		assert.equal(section.start.line, 1);
-		assert.equal(section.start.column, 2);
-		assert.equal(section.start.offset, 1);
-		assert.equal(section.end.line, 1);
-		assert.equal(section.end.column, 6);
-		assert.equal(section.end.offset, 5);
+		assert.equal(section?.text, 'sec1');
+		assert.equal(section?.start.line, 1);
+		assert.equal(section?.start.column, 2);
+		assert.equal(section?.start.offset, 1);
+		assert.equal(section?.end.line, 1);
+		assert.equal(section?.end.column, 6);
+		assert.equal(section?.end.offset, 5);
 
 		let direction = nodes.find(n => n.label === 'direction');
-		assert.equal(direction.text, 'direction1');
-		assert.equal(direction.start.line, 5);
-		assert.equal(direction.start.column, 9);
-		assert.equal(direction.start.offset, 28);
-		assert.equal(direction.end.line, 5);
-		assert.equal(direction.end.column, 19);
-		assert.equal(direction.end.offset, 38);
+		assert.equal(direction?.text, 'direction1');
+		assert.equal(direction?.start.line, 5);
+		assert.equal(direction?.start.column, 9);
+		assert.equal(direction?.start.offset, 28);
+		assert.equal(direction?.end.line, 5);
+		assert.equal(direction?.end.column, 19);
+		assert.equal(direction?.end.offset, 38);
 
 	});
 });
