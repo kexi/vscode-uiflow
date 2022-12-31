c85a00fa58519a63cc33bf4f54a2be62dad6e116
Kei Nakayama <kei.of.nakayama@gmail.com>
2022-12-31 17:50:23 +0900

    fix

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,8 +1,8 @@
 'use strict'
 
-import uiflow, { ErrorHandler } from '@kexi/uiflow'
+import uiflow, {ErrorHandler} from '@kexi/uiflow'
 import * as through2 from 'through2'
-import { Format } from '@kexi/uiflow/dist/cjs/app/interfaces'
+import {Format} from '@kexi/uiflow/dist/cjs/app/interfaces'
 
 export class Compiler {
   public buildWithCode(
@@ -26,10 +26,11 @@ export class Compiler {
   ): Promise<Buffer> {
     const promise = new Promise<Buffer>((_resolve, _reject) => {
       const buff: Uint8Array[] = []
-      const output = through2((chunk: any, enc: string, callback: Function) => {
+      const output = through2((chunk: Uint8Array, enc: string, callback: Function) => {
         buff.push(chunk)
         callback()
       })
+
       const stream = this.buildWithCode(fileName, code, format, (e: any) => {
         _reject(e)
       })
